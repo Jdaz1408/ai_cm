5740f603593be70366e2f9457535be7db3935d01
Elido99 <arroyavejuan36@gmail.com>
2025-02-16 17:14:00 -0500

    Update next.config.js with app directory support

--- next.config.js
+++ next.config.js
@@ -1,12 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true
+  },
   images: {
     unoptimized: true,
   },
   output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
-  },
+  }
 }
 
 module.exports = nextConfig
